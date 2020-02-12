44331195ddcd6c048d5c1f538cb57973a21b1928
Tomas Farago <sensej007@email.cz>
2020-02-12 14:37:48 +0100

    Use *.flow file pattern in FlowScene save/load
    so that the mathing files are displayed in the dialogs.

--- qtpynodeeditor/flow_scene.py
+++ qtpynodeeditor/flow_scene.py
@@ -94,7 +94,7 @@ class _FlowSceneModel:
         if file_name is None:
             file_name, _ = QFileDialog.getSaveFileName(
                 None, "Open Flow Scene", QDir.homePath(),
-                "Flow Scene Files (.flow)")
+                "Flow Scene Files (*.flow)")
 
         if file_name:
             file_name = str(file_name)
@@ -108,7 +108,7 @@ class _FlowSceneModel:
         if file_name is None:
             file_name, _ = QFileDialog.getOpenFileName(
                 None, "Open Flow Scene", QDir.homePath(),
-                "Flow Scene Files (.flow)")
+                "Flow Scene Files (*.flow)")
 
         if not os.path.exists(file_name):
             return
